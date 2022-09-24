92b6d31b9f3c54006df0aba721fa773b9a98ed7c
Tyler Moore <tyler.moore@codeclaritysoftware.com>
2022-09-24 14:14:03 -0500

    solved easy first

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,6 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [] ? never : T[0];
+
+/* I learned:
+1. Conditional types can return never to 'guard' certain scenarios.
+2. Conditions can continue to narrow types beyond the lefthand side 'extends'.
+*/
